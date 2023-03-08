c8fbe7370201d05371dc7be8c911f33d0b8747c2
lynn <lynn@Admins-MacBook-Pro.local>
2023-03-08 18:12:15 -0500

    working on app.py not connecting properly

--- app.py
+++ app.py
@@ -45,16 +45,8 @@ def welcome():
         f"Available Routes:<br/>"
     )
 
-@app.route("/api/v1.0/names")
-def names():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World!'
-
-
+#query results from precipiation analysis only last 12 months
 @app.route('/api/v1.0/precipitation')
 def precipitation():
     session = Session()
@@ -66,6 +58,46 @@ def precipitation():
         prcp_dict[date] = prcp
     
     return jsonify(prcp_dict)
+
+
+
+#Return a JSON list of stations from the dataset
+@app.route('/api/v1.0/stations')
+def stations():
+    session = Session()
+    station = session.query(Station.station).all()
+    session.close()
+
+    stations_list = list(np.travel(results))
+    return jsonify(stations_list)
+
+
+#Query the dates and temperature observations of the most-active station for the previous year of data.
+#Return a JSON list of temperature observations for the previous year.
+@app.route('/api/v1.0/tobs')
+def most_active():
+    session = Session()
+    most_active = session.query(Measurement.station, func.count(Measurement.station)).\
+              group_by(Measurement.station).\
+              order_by(func.count(Measurement.station).desc()).all()
+    session.close()
+
+# Select the station with the highest number of temperature observations
+    most_active_station = most_active[0][0]
+
+# Query the most active station's temperature data for the previous year
+    temps_last_year = session.query(Measurement.tobs).\
+                      filter(Measurement.station == most_active_station).\
+                      filter(Measurement.date >= '2016-08-23').all()
+
+# Convert the query results to a list
+    temps_list = list(np.ravel(temps_last_year))
+
+    # Return the JSON representation of the list
+    return jsonify(temps_list)
+
+
+    session.close()
 
 
 if __name__ == '__main__':
